8e167ce2a9d7302f49f5928296d190fc8efda563
Danny Leshem <dleshem@gmail.com>
2016-10-01 22:36:33 +0300

    Now exports antiPlagiarism function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import * as ImageTypes from './ImageTypes'
 import {KotarClient} from './KotarClient'
+import antiPlagiarism from './AntiPlagiarism'
 
 export {ImageTypes as ImageTypes}
 export {KotarClient as KotarClient}
+export {antiPlagiarism as antiPlagiarism}
